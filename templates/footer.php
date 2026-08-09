41bf70fcced01aa04266bf77cbbb5dbcd09100d4
AbdurRahman Dot Top <abdurrahmandottopofficial@gmail.com>
2026-08-09 08:44:13 +0530

    Add Tawk.to live chat script and a left-aligned Back to Top button

--- templates/footer.php
+++ templates/footer.php
@@ -79,7 +79,27 @@
     </div>
 </footer>
 
+<!-- Back to Top Button -->
+<button id="back-to-top" class="back-to-top" aria-label="Back to Top">
+    <i class='bx bx-up-arrow-alt'></i>
+</button>
+
 <!-- Main JS -->
 <script src="<?= BASE_URL ?>assets/js/main.js"></script>
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/61f0d07fb9e4e21181bbeec3/1fqaastd0';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
 </body>
 </html>
